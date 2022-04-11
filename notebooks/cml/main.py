0369e819cb4836f2a4199a0bd73732d9c4838e76
Polz <julius.polz@kit.edu>
2022-04-11 13:04:24 +0000

    now with era5 data

--- notebooks/cml/main.py
+++ notebooks/cml/main.py
@@ -21,7 +21,7 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import Band, CheckboxButtonGroup, Button, ColumnDataSource, PreText, Select, CustomJS
+from bokeh.models import Band, CheckboxButtonGroup, Button, ColumnDataSource, PreText, Select, CustomJS, Legend
 from bokeh.plotting import figure
 from bokeh.events import ButtonClick
 import xarray as xr
@@ -30,41 +30,39 @@ import netCDF4
 
 DATA_DIR = join(dirname(__file__), 'daily')
 
-MONTHS = [s.split('/')[-1] for s in glob.glob('../data/raw*')]
+MONTHS = [s.split('_')[-1].split('.')[0] for s in glob.glob('/pd/data/CML/data/reference/anomaly_flags/2022.01.BY1412_2_BY1298_2_2019_*')]
 MONTHS.sort()
-DEFAULT_TICKERS = xr.open_dataset('../data/raw01.nc').cml_id.values
-# print(DEFAULT_TICKERS)
-
-def nix(val, lst):
-#     return [x for x in lst if x != val]
-    return [x for x in lst]
-
-# @lru_cache()
-def load_ticker(cml_id, month):
-    data = xr.open_dataset('../data/'+month).sel(cml_id=cml_id).isel(channel_id=0).load()
-    data = data.to_pandas()
-    data['date'] = data.index
-    data = data.rename(columns={'txrx':'trsl1', 'rainfall_amount':'R'})
-    df2 = xr.open_dataset('../data/'+month).sel(cml_id=cml_id).isel(channel_id=1).txrx.to_pandas()
-    data.loc[:,'trsl2'] = df2.values
-    data = data.set_index('date')
-    return data
+DEFAULT_TICKERS = list(xr.open_dataset('/pd/data/CML/data/reference/anomaly_flags/2022.01.cml_list.nc').cml_id.values)
 
 # @lru_cache()
 def get_data(cml_id, month):
-    data = load_ticker(cml_id, month)
-#     print(data.head())
+    ds = xr.open_dataset('/pd/data/CML/data/reference/anomaly_flags/2022.01.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=0).load()
+    data = ds.to_pandas()
+    ds.close()
+    data['date'] = data.index
+    data = data.rename(columns={'txrx':'trsl1', 'rainfall_amount':'R'})
+    ds2 = xr.open_dataset('/pd/data/CML/data/reference/anomaly_flags/2022.01.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=1).txrx.load()
+    df2 = ds2.to_pandas()
+    ds2.close()
+    trsl2 = df2.values.copy()
+    data.loc[:,'trsl2'] = trsl2
+    data = data.set_index('date')
+    for i, label in enumerate(LABELS):
+        val = data[label].values.copy()
+        data.loc[:,label] = val*(i+1)/len(LABELS)
     return data
 
 
 # set up widgets
 
 stats = PreText(text='', width=500)
-ticker1 = Select(value='raw01.nc', options=nix('raw01.nc', MONTHS))
-ticker2 = Select(value='BY0081_2_BY1150_2', options=nix('BY0081_2_BY1150_2', DEFAULT_TICKERS))
+ticker1 = Select(value=MONTHS[0], options=MONTHS)
+ticker2 = Select(value=DEFAULT_TICKERS[0], options=DEFAULT_TICKERS)
 
-button1 = Button(label="Save CML flags", button_type="success")
-button2 = Button(label="Delete CML flags", button_type="success")
+button1 = Button(label="Save flags", button_type="success")
+button2 = Button(label="Delete all flags", button_type="success")
+button2a = Button(label="Delete selected type of flags", button_type="success")
+button2b = Button(label="Delete selected type of flags", button_type="success")
 button3 = Button(label="Flag period", button_type="success")
 LABELS = ["Dry", "Wet", "Anomaly", "Dew", "LOL"]
 
@@ -75,7 +73,12 @@ checkbox_button_group.js_on_click(CustomJS(code="""
 
 def callback1(event):
     data = source.to_df()
-    print(data.head())
+    cml_id = ticker2.value
+    month = ticker1.value
+    with netCDF4.Dataset('/pd/data/CML/data/reference/anomaly_flags/2022.01.'+cml_id+'_2019_'+month+'.nc', 'a') as nc_fh:
+        for label in LABELS:
+            nc_fh[label][:] = (data[label]>0).astype(bool)
+    print('saved')
 
 def callback2(event):
     for label in LABELS:
@@ -83,6 +86,23 @@ def callback2(event):
             label : [ (slice(0,None), np.zeros_like(source.data[label])) ],
         }
 
+        source.patch(patches)
+        
+def callback2a(event):
+    ind = checkbox_button_group.to_json(True)['active']
+    for i, label in enumerate(LABELS):
+        if i in ind:
+            patches = {
+                label : [ (slice(0,None), np.zeros_like(source.data[label])) ],
+            }
+            source.patch(patches)
+        
+def callback2b(event):
+    selected = source.selected.indices
+    for i, label in enumerate(LABELS):
+        patches = {
+            label : [(s,0) for s in selected],
+        }
         source.patch(patches)
 
 def callback3(event):
@@ -98,6 +118,8 @@ def callback3(event):
 
 button1.on_event(ButtonClick, callback1)
 button2.on_event(ButtonClick, callback2)
+button2a.on_event(ButtonClick, callback2a)
+button2a.on_event(ButtonClick, callback2b)
 button3.on_event(ButtonClick, callback3)
 
 # set up plots
@@ -106,19 +128,18 @@ source = ColumnDataSource(data=dict(date=[], t1=[], t2=[],))
 source_static = ColumnDataSource(data=dict(date=[], t1=[], t2=[],))
 tools = 'pan,wheel_zoom,xbox_select,reset,box_zoom'
 
-# corr = figure(width=250, height=250,
-#               tools='pan,box_zoom,box_select,reset')
-# corr.circle('trsl1', 'R', size=2, source=source,
-#             selection_color="orange", alpha=1, nonselection_alpha=1, selection_alpha=1)
+corr = figure(width=250, height=250,
+              tools='pan,box_zoom,box_select,reset')
+corr.circle('trsl1', 'R', size=2, source=source,
+            selection_color="orange", alpha=1, nonselection_alpha=1, selection_alpha=1)
 
 ts1 = figure(width=1800, height=250, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts1.line('date', 'trsl1', legend_label="channel 1", source=source_static)
 ts1.circle('date', 'trsl1', size=5, source=source, color=None, selection_color="green")
 ts1.line('date', 'trsl2', legend_label="channel 2", source=source_static, color="gray")
-ts1.circle('date', 'trsl2', size=2, source=source, color=None, selection_color="green")
 
 
-ts2 = figure(width=1800, height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select", y_range=(0.1, 1.1))
+ts2 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select", y_range=(0.1, 1.1))
 colors = ['red','blue','pink','gray', 'orange']
 for i, label in enumerate(LABELS):
 #     ts2.line('date', y=source.data[label]*i/len(LABELS), legend_label=label, color=colors[i], source=source, line_width=10)
@@ -129,20 +150,38 @@ for i, label in enumerate(LABELS):
             fill_alpha=1.0, line_width=1, line_color=colors[i])
     ts1.add_layout(band)
 ts2.x_range = ts1.x_range
+ts2.legend.orientation = "horizontal"
+ts2.legend.location = "top_right"
+ts2.legend.border_line_color = None
+ts2.legend.background_fill_alpha = 0
 
-ts3 = figure(width=1800, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts3 = figure(width=1800, height=250, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts3.x_range = ts1.x_range
-ts3.line('date', 'R', source=source_static)
-ts3.circle('date', 'R', size=2, source=source, color=None, selection_color="green")
+ts3.line('date', 'R', source=source_static, color='red', legend_label="R_CML")
+ts3.line('date', 'RADOLAN_RW', source=source_static, color='blue', legend_label="RADOLAN-RW")
+ts3.line('date', 'RADKLIM_YW', source=source_static, color='green', legend_label="RADKLIM-YW")
+
+ts4 = figure(width=1800, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts4.x_range = ts1.x_range
+ts4.line('date', 'era5_t2m', source=source_static, legend_label="Surface temperature", color='red')
+ts4.line('date', 'era5_d2m', source=source_static, legend_label="Dew point tempreature")
+
+ts5 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts5.x_range = ts1.x_range
+ts5.line('date', 'era5_wind_speed', source=source_static)
+
+ts6 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts6.x_range = ts1.x_range
+ts6.line('date', 'era5_ssrd', source=source_static)
 
 # set up callbacks
 
 def ticker1_change(attrname, old, new):
-    ticker2.options = nix(new, DEFAULT_TICKERS)
+    ticker2.options = DEFAULT_TICKERS
     update()
 
 def ticker2_change(attrname, old, new):
-    ticker2.options = nix(new, DEFAULT_TICKERS)
+    ticker2.options = DEFAULT_TICKERS
     update()
 
 def update(selected=None):
@@ -154,8 +193,8 @@ def update(selected=None):
 
     update_stats(data, ['trsl1', 'trsl2', 'R'])
 
-#     corr.title.text = 'TRSL channel 1 vs. Path averaged radar rainfall'
-    ts1.title.text, ts2.title.text, ts3.title.text = 'TRSL channel 1', 'Anomaly', 'Path averaged radar rainfall'
+#     corr.title.text = 'TRSL channel 1 vs. Path averaged rainfall'
+#     ts1.title.text, ts2.title.text, ts3.title.text = 'Transmitted minus received signal level [dB]', 'Anomaly flags', 'Path averaged CML rainfall [mm/h] after combined methods from Graf et al. 2020 and Polz et al. 2020'
 
 def update_stats(data, c_list):
     stats.text = str(data[c_list].describe())
@@ -176,13 +215,15 @@ def selection_change(attrname, old, new):
 source.selected.on_change('indices', selection_change)
 
 # set up layout
-widgets = column(ticker1, ticker2, button1, button2)
-main_row = row(widgets, 
-               stats, 
-#                corr,
-              )
-series = column(ts1, ts2, ts3)
-layout = column(main_row, button3, checkbox_button_group, series)
+widgets = column(ticker1, ticker2,button1, button3)
+buttons2 = column(button2, button2a, button2b)
+main_row = row(widgets, stats,buttons2, corr,)
+series = column(ts1, ts2, ts3,
+                ts4, 
+                ts5, 
+                ts6
+               )
+layout = column(main_row, checkbox_button_group, series)
 
 # initialize
 update()
