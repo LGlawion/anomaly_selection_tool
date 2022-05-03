6dbac4bcf267b62a17f8ea6c49130c13f7c7a28b
Polz <julius.polz@kit.edu>
2022-05-03 15:01:37 +0000

    new layout and environment

--- notebooks/cml/main.py
+++ notebooks/cml/main.py
@@ -21,7 +21,7 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import Band, CheckboxButtonGroup, Button, ColumnDataSource, PreText, Select, CustomJS, Legend
+from bokeh.models import Band, CheckboxButtonGroup, Button, ColumnDataSource, PreText, Select, CustomJS, Legend, LinearAxis, Range1d
 from bokeh.plotting import figure
 from bokeh.events import ButtonClick
 import xarray as xr
@@ -37,6 +37,7 @@ DEFAULT_TICKERS = list(xr.open_dataset('/pd/data/CML/data/reference/anomaly_flag
 # @lru_cache()
 def get_data(cml_id, month):
     ds = xr.open_dataset('/pd/data/CML/data/reference/anomaly_flags/2022.01.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=0).load()
+    meta = str(np.round(ds.length.values, decimals=2))+' '+str(np.round(ds.frequency.values/1e9, decimals=2))
     data = ds.to_pandas()
     ds.close()
     data['date'] = data.index
@@ -50,7 +51,7 @@ def get_data(cml_id, month):
     for i, label in enumerate(LABELS):
         val = data[label].values.copy()
         data.loc[:,label] = val*(i+1)/len(LABELS)
-    return data
+    return data, meta
 
 
 # set up widgets
@@ -64,7 +65,7 @@ button2 = Button(label="Delete all flags", button_type="success")
 button2a = Button(label="Delete selected type of flags", button_type="success")
 button2b = Button(label="Delete selected type of flags", button_type="success")
 button3 = Button(label="Flag period", button_type="success")
-LABELS = ["Dry", "Wet", "Anomaly", "Dew", "LOL"]
+LABELS = ["Jump", "Dew", "Diurnal pattern", "Snow", "Strong fluctuation", "Light fluctuation", "Unknown anomaly"]
 
 checkbox_button_group = CheckboxButtonGroup(labels=LABELS, active=[])
 checkbox_button_group.js_on_click(CustomJS(code="""
@@ -128,24 +129,16 @@ source = ColumnDataSource(data=dict(date=[], t1=[], t2=[],))
 source_static = ColumnDataSource(data=dict(date=[], t1=[], t2=[],))
 tools = 'pan,wheel_zoom,xbox_select,reset,box_zoom'
 
-corr = figure(width=250, height=250,
-              tools='pan,box_zoom,box_select,reset')
-corr.circle('trsl1', 'R', size=2, source=source,
-            selection_color="orange", alpha=1, nonselection_alpha=1, selection_alpha=1)
-
-ts1 = figure(width=1800, height=250, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts1 = figure(width=1900, height=250, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts1.line('date', 'trsl1', legend_label="channel 1", source=source_static)
 ts1.circle('date', 'trsl1', size=5, source=source, color=None, selection_color="green")
 ts1.line('date', 'trsl2', legend_label="channel 2", source=source_static, color="gray")
 
 
-ts2 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select", y_range=(0.1, 1.1))
-colors = ['red','blue','pink','gray', 'orange']
+ts2 = figure(width=1900, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select", y_range=(0.1, 1.1))
+colors = ['red','blue','pink','gray', 'orange', 'green', 'black']
 for i, label in enumerate(LABELS):
-#     ts2.line('date', y=source.data[label]*i/len(LABELS), legend_label=label, color=colors[i], source=source, line_width=10)
-#     print(source.data[label])
-#     ts2.circle('date', y=source.data[label]*i/len(LABELS), size=10, source=source, color=colors[i], selection_color="green")
-    ts2.circle('date', label, size=10, source=source, legend_label=label, color=colors[i])
+    ts2.circle('date', label, size=7, source=source, legend_label=label, color=colors[i])
     band = Band(base='date', lower=0, upper=label, source=source, level='underlay',
             fill_alpha=1.0, line_width=1, line_color=colors[i])
     ts1.add_layout(band)
@@ -155,24 +148,35 @@ ts2.legend.location = "top_right"
 ts2.legend.border_line_color = None
 ts2.legend.background_fill_alpha = 0
 
-ts3 = figure(width=1800, height=250, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts3 = figure(width=1900, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts3.x_range = ts1.x_range
 ts3.line('date', 'R', source=source_static, color='red', legend_label="R_CML")
 ts3.line('date', 'RADOLAN_RW', source=source_static, color='blue', legend_label="RADOLAN-RW")
 ts3.line('date', 'RADKLIM_YW', source=source_static, color='green', legend_label="RADKLIM-YW")
+ts3.legend.location = "top_right"
+ts3.legend.border_line_color = None
+ts3.legend.background_fill_alpha = 0
 
-ts4 = figure(width=1800, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts4 = figure(width=1900, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts4.x_range = ts1.x_range
 ts4.line('date', 'era5_t2m', source=source_static, legend_label="Surface temperature", color='red')
 ts4.line('date', 'era5_d2m', source=source_static, legend_label="Dew point tempreature")
+ts4.legend.location = "top_right"
+ts4.legend.border_line_color = None
+ts4.legend.background_fill_alpha = 0
 
-ts5 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts5 = figure(width=1900, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts5.y_range = Range1d(0, 10)
+# ts5.y_range = Range1d()
+ts5.extra_y_ranges = {"foo": Range1d(start=0, end=900)}
+# ts5.extra_y_ranges = {"foo": Range1d()}
+ts5.add_layout(LinearAxis(y_range_name="foo"), 'right')
 ts5.x_range = ts1.x_range
-ts5.line('date', 'era5_wind_speed', source=source_static)
-
-ts6 = figure(width=1800, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
-ts6.x_range = ts1.x_range
-ts6.line('date', 'era5_ssrd', source=source_static)
+ts5.line('date', 'era5_wind_speed', source=source_static, legend_label="Wind speed")
+ts5.line('date', 'era5_ssrd', source=source_static, y_range_name="foo", color='red', legend_label="Surface solar radiation")
+ts5.legend.location = "top_right"
+ts5.legend.border_line_color = None
+ts5.legend.background_fill_alpha = 0
 
 # set up callbacks
 
@@ -187,41 +191,41 @@ def ticker2_change(attrname, old, new):
 def update(selected=None):
     cml_id = ticker2.value
     month = ticker1.value
-    data = get_data(cml_id, month)
+    data, meta = get_data(cml_id, month)
     source.data = data
     source_static.data = data
 
-    update_stats(data, ['trsl1', 'trsl2', 'R'])
+    update_stats(data, meta, ['trsl1', 'trsl2', 'R'])
 
 #     corr.title.text = 'TRSL channel 1 vs. Path averaged rainfall'
 #     ts1.title.text, ts2.title.text, ts3.title.text = 'Transmitted minus received signal level [dB]', 'Anomaly flags', 'Path averaged CML rainfall [mm/h] after combined methods from Graf et al. 2020 and Polz et al. 2020'
 
-def update_stats(data, c_list):
-    stats.text = str(data[c_list].describe())
+def update_stats(data, meta, c_list):
+    stats.text = str(data[c_list].describe())+'   '+meta
 
 ticker1.on_change('value', ticker1_change)
 ticker2.on_change('value', ticker2_change)
 
 def selection_change(attrname, old, new):
-#     cml_id = ticker2.value
-#     month = ticker1.value
-#     data = get_data(cml_id, month)
+    cml_id = ticker2.value
+    month = ticker1.value
+    _, meta = get_data(cml_id, month)
     data = source.to_df()
     selected = source.selected.indices
     if selected:
         data = data.iloc[selected, :]
-    update_stats(data, ['trsl1', 'trsl2', 'R'])
+    update_stats(data, meta, ['trsl1', 'trsl2', 'R'])
 
 source.selected.on_change('indices', selection_change)
 
 # set up layout
 widgets = column(ticker1, ticker2,button1, button3)
 buttons2 = column(button2, button2a, button2b)
-main_row = row(widgets, stats,buttons2, corr,)
+main_row = row(widgets, stats,buttons2,)
 series = column(ts1, ts2, ts3,
                 ts4, 
                 ts5, 
-                ts6
+#                 ts6
                )
 layout = column(main_row, checkbox_button_group, series)
 
