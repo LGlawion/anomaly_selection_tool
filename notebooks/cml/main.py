d14cfd33fb747640b0c899dd0ff6bf0b3f224d8e
Julius Polz <julius.polz@kit.edu>
2022-07-20 16:13:27 +0200

    new classes, scaling and data

--- notebooks/cml/main.py
+++ notebooks/cml/main.py
@@ -27,22 +27,22 @@ from bokeh.events import ButtonClick
 import xarray as xr
 import glob
 import netCDF4
-
+import time
 # DATA_DIR = join(dirname(__file__), 'daily')
 
-MONTHS = list(np.unique([s.split('_')[-1].split('.')[0] for s in glob.glob('data/2022.01.*_2019_*')]))
+MONTHS = list(np.unique([s.split('_')[-1].split('.')[0] for s in glob.glob('data/2022.02.*_2019_*')]))
 MONTHS.sort()
-DEFAULT_TICKERS = list(xr.open_dataset('data/2022.01.cml_list.nc').cml_id.values)
+DEFAULT_TICKERS = list(xr.open_dataset('data/2022.02.cml_list.nc').cml_id.values)
 
 # @lru_cache()
 def get_data(cml_id, month):
-    ds = xr.open_dataset('data/2022.01.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=0).load()
+    ds = xr.open_dataset('data/2022.02.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=0).load()
     meta = str(np.round(ds.length.values, decimals=2))+' '+str(np.round(ds.frequency.values/1e9, decimals=2))
     data = ds.to_pandas()
     ds.close()
     data['date'] = data.index
     data = data.rename(columns={'txrx':'trsl1', 'rainfall_amount':'R'})
-    ds2 = xr.open_dataset('data/2022.01.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=1).txrx.load()
+    ds2 = xr.open_dataset('data/2022.02.'+cml_id+'_2019_'+month+'.nc').isel(channel_id=1).txrx.load()
     df2 = ds2.to_pandas()
     ds2.close()
     trsl2 = df2.values.copy()
@@ -60,12 +60,12 @@ stats = PreText(text='', width=500)
 ticker1 = Select(value=MONTHS[0], options=MONTHS)
 ticker2 = Select(value=DEFAULT_TICKERS[0], options=DEFAULT_TICKERS)
 
-button1 = Button(label="Save flags", button_type="success")
-button2 = Button(label="Delete all flags", button_type="success")
-button2a = Button(label="Delete selected type of flags", button_type="success")
-button2b = Button(label="Delete flags in current selection", button_type="success")
-button3 = Button(label="Flag period", button_type="success")
-LABELS = ["Jump", "Dew", "Diurnal pattern", "Snow", "Strong fluctuation", "Light fluctuation", "Unknown anomaly"]
+button1 = Button(label="Save flags", button_type="success", height=25)
+button2 = Button(label="Delete all flags", button_type="success", height=25)
+button2a = Button(label="Delete selected type of flags", button_type="success", height=25)
+button2b = Button(label="Delete flags in current selection", button_type="success", height=25)
+button3 = Button(label="Flag period", button_type="success", height=25)
+LABELS = ["Jump", "Dew", "Fluctuation", "Unknown anomaly"]
 
 checkbox_button_group = CheckboxButtonGroup(labels=LABELS, active=[])
 checkbox_button_group.js_on_click(CustomJS(code="""
@@ -76,7 +76,7 @@ def callback1(event):
     data = source.to_df()
     cml_id = ticker2.value
     month = ticker1.value
-    with netCDF4.Dataset('data/2022.01.'+cml_id+'_2019_'+month+'.nc', 'a') as nc_fh:
+    with netCDF4.Dataset('data/2022.02.'+cml_id+'_2019_'+month+'.nc', 'a') as nc_fh:
         for label in LABELS:
             nc_fh[label][:] = (data[label]>0).astype(bool)
     print('saved')
@@ -134,7 +134,6 @@ ts1.line('date', 'trsl1', legend_label="channel 1", source=source_static)
 ts1.circle('date', 'trsl1', size=5, source=source, color=None, selection_color="green")
 ts1.line('date', 'trsl2', legend_label="channel 2", source=source_static, color="gray")
 
-
 ts2 = figure(width=1900, height=100, tools=tools, x_axis_type='datetime', active_drag="xbox_select", y_range=(0.1, 1.1))
 colors = ['red','blue','pink','gray', 'orange', 'green', 'black']
 for i, label in enumerate(LABELS):
@@ -160,6 +159,7 @@ ts3.legend.background_fill_alpha = 0
 ts4 = figure(width=1900, height=150, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
 ts4.x_range = ts1.x_range
 ts4.line('date', 'era5_t2m', source=source_static, legend_label="Surface temperature", color='red')
+ts4.line('date', y=0, source=source_static, legend_label="Zero degrees", color='purple')
 ts4.line('date', 'era5_d2m', source=source_static, legend_label="Dew point tempreature")
 ts4.legend.location = "top_right"
 ts4.legend.border_line_color = None
@@ -183,10 +183,35 @@ ts5.legend.background_fill_alpha = 0
 def ticker1_change(attrname, old, new):
     ticker2.options = DEFAULT_TICKERS
     update()
+#     ts1.y_range = Range1d(np.nanmin([np.nanmin(pd.DataFrame(source.data)['trsl1'])-5, 
+#                                      np.nanmin(pd.DataFrame(source.data)['trsl2'])-5,
+#                                      50
+#                                     ]), 
+#                           np.nanmax([np.nanmax(pd.DataFrame(source.data)['trsl1'])+5, 
+#                                      np.nanmax(pd.DataFrame(source.data)['trsl2'])+5,
+#                                      70
+#                                     ]))
 
 def ticker2_change(attrname, old, new):
     ticker2.options = DEFAULT_TICKERS
     update()
+#     ts1.y_range = Range1d(start = np.nanmin([np.nanmin(pd.DataFrame(source.data)['trsl1'])-5, 
+#                                      np.nanmin(pd.DataFrame(source.data)['trsl2'])-5,
+#                                      50
+#                                     ]), 
+#                           end = np.nanmax([np.nanmax(pd.DataFrame(source.data)['trsl1'])+5, 
+#                                      np.nanmax(pd.DataFrame(source.data)['trsl2'])+5,
+#                                      70
+#                                     ]),
+#                           reset_start = np.nanmin([np.nanmin(pd.DataFrame(source.data)['trsl1'])-5, 
+#                                      np.nanmin(pd.DataFrame(source.data)['trsl2'])-5,
+#                                      50
+#                                     ]), 
+#                           reset_end = np.nanmax([np.nanmax(pd.DataFrame(source.data)['trsl1'])+5, 
+#                                      np.nanmax(pd.DataFrame(source.data)['trsl2'])+5,
+#                                      70
+#                                     ]),
+#                          )
 
 def update(selected=None):
     cml_id = ticker2.value
@@ -194,6 +219,15 @@ def update(selected=None):
     data, meta = get_data(cml_id, month)
     source.data = data
     source_static.data = data
+#     ts1.y_range.destroy()
+    ts1.y_range.update(start=np.nanmin([np.nanmin(pd.DataFrame(source.data)['trsl1'])-2, 
+                                     np.nanmin(pd.DataFrame(source.data)['trsl2'])-2,
+                                     60
+                                    ]), 
+                          end=np.nanmax([np.nanmax(pd.DataFrame(source.data)['trsl1'])+2, 
+                                     np.nanmax(pd.DataFrame(source.data)['trsl2'])+2,
+                                     70
+                                    ]))
 
     update_stats(data, meta, ['trsl1', 'trsl2', 'R'])
 
@@ -201,7 +235,8 @@ def update(selected=None):
 #     ts1.title.text, ts2.title.text, ts3.title.text = 'Transmitted minus received signal level [dB]', 'Anomaly flags', 'Path averaged CML rainfall [mm/h] after combined methods from Graf et al. 2020 and Polz et al. 2020'
 
 def update_stats(data, meta, c_list):
-    stats.text = str(data[c_list].describe())+'   '+meta
+#     print(data[c_list].describe().head(4))
+    stats.text = str(data[c_list].describe().head(4))+'  Length: '+meta[0]+'km, Frequency: '+meta[1]+'GHz'
 
 ticker1.on_change('value', ticker1_change)
 ticker2.on_change('value', ticker2_change)
@@ -219,9 +254,10 @@ def selection_change(attrname, old, new):
 source.selected.on_change('indices', selection_change)
 
 # set up layout
-widgets = column(ticker1, ticker2,button1, button3)
+widgets = column(ticker1, ticker2)
+buttons1 = column(button1, button3)
 buttons2 = column(button2, button2a, button2b)
-main_row = row(widgets, stats,buttons2,)
+main_row = row(widgets, stats, buttons1, buttons2,)
 series = column(ts1, ts2, ts3,
                 ts4, 
                 ts5, 
